ccf4cff2df60491b1ec9c017661e2d7763b27962
Donald Whitely <dswhitely1@gmail.com>
2019-11-21 20:45:34 -0500

    Fixing Database Entries

--- data/migrations/20190622212629_articles_table.js
+++ data/migrations/20190622212629_articles_table.js
@@ -1,25 +1,26 @@
-exports.up = function(knex, Promise) {
-  return knex.schema.createTable("articles", articles => {
-    articles.increments();
+exports.up = function (knex, Promise) {
+    return knex.schema.createTable("articles", articles => {
+        articles.increments();
 
-    articles.string("title", 250).notNullable();
+        articles.string("title", 250).notNullable();
 
-    articles.string("summary", 1000).notNullable();
+        articles.string("summary", 1000).notNullable();
 
-    articles.string("link", 1000);
+        articles.string("link", 1000);
 
-    articles.string("image", 1000);
+        articles.string("image", 1000);
 
-    articles
-      .integer("user_id")
-      .unsigned()
-      .references("id")
-      .inTable("users");
+        articles
+            .integer("user_id")
+            .unsigned()
+            .references("id")
+            .inTable("users");
+        articles.integer("category_id").unsigned().references('id').inTable('category');
 
-    articles.string("category", 250);
-  });
+        articles.string("category", 250);
+    });
 };
 
-exports.down = function(knex, Promise) {
-  return knex.schema.dropTableIfExists("articles");
+exports.down = function (knex, Promise) {
+    return knex.schema.dropTableIfExists("articles");
 };
