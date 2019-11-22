ee6a7ff790dc92b9430c9e9fdfb6617c90552287
Justin Trombley <jtrombley96@gmail.com>
2019-11-21 19:41:35 -0500

    Articles Added

--- data/seeds/003-articles.js
+++ data/seeds/003-articles.js
@@ -1,47 +1,103 @@
-exports.seed = function(knex, Promise) {
-  // Deletes ALL existing entries
-  return knex("articles")
-    .del()
-    .then(function() {
-      // Inserts seed entries
-      return knex("articles").insert([
-        {
-          id: 1,
-          title: "Pokemon electronic game",
-          summary:
-            "Pokémon, electronic game series from Nintendo that debuted in Japan in 1995 and later became wildly popular in the United States. The series, originally produced for the company’s Game Boy line of handheld consoles, was introduced in 1998 to the United States with two titles, known to fans as Red and Blue.",
-          link:
-            "https://www.britannica.com/topic/Pokemon-electronic-game",
-          image:
-            "https://cdn.britannica.com/s:700x450/70/122270-004-0564DF2A.jpg",
-          user_id: 1,
-          category_id: 1,
-          category: "Game"
-        },
-        {
-          id: 2,
-          title: "Dota 2",
-          summary:
-            "Dota 2 is a multiplayer online battle arena video game developed and published by Valve Corporation. The game is a sequel to Defense of the Ancients, which was a community-created mod for Blizzard Entertainment's Warcraft III: Reign of Chaos and its expansion pack, The Frozen Throne",
-          link: "https://en.wikipedia.org/wiki/Dota_2",
-          image:
-            "https://dotesports-media.nyc3.cdn.digitaloceanspaces.com/wp-content/uploads/2019/02/24144435/0_vbw4wQW_Xq2_3eOo.jpg",
-          user_id: 2,
-          category_id: 2,
-          category: "Game"
-        },
-        {
-          id: 3,
-          title: "League of Legends",
-          summary:
-            "League of Legends is a multiplayer online battle arena video game developed and published by Riot Games for Microsoft Windows and macOS. The game follows a freemium model and is supported by microtransactions, and was inspired by the Warcraft III: The Frozen Throne mod, Defense of the Ancients",
-          link: "https://en.wikipedia.org/wiki/League_of_Legends",
-          image:
-            "https://cdn.arstechnica.net/wp-content/uploads/2015/02/lol-640x360.png",
-          user_id: 3,
-          category_id: 3,
-          category: "Game"
-        }
-      ]);
-    });
+exports.seed = function (knex, Promise) {
+	// Deletes ALL existing entries
+	return knex('articles').del().then(function () {
+		// Inserts seed entries
+		return knex('articles').insert([
+			{
+				id            : 1,
+				title         : 'Permanent Record',
+				summary       :
+					'Pokémon, electronic game series from Nintendo that debuted in Japan in 1995 and later became wildly popular in the United States. The series, originally produced for the company’s Game Boy line of handheld consoles, was introduced in 1998 to the United States with two titles, known to fans as Red and Blue.',
+				link          : 'https://www.britannica.com/topic/Pokemon-electronic-game',
+				image         :
+					'https://images-na.ssl-images-amazon.com/images/I/51z1ZaEn6sL._SX327_BO1,204,203,200_.jpg',
+				user_id       : 1,
+				category_name : 'Article',
+				category_id   : '1',
+			},
+			{
+				id            : 2,
+				title         : 'The Design Hustle',
+				summary       :
+					"Dota 2 is a multiplayer online battle arena video game developed and published by Valve Corporation. The game is a sequel to Defense of the Ancients, which was a community-created mod for Blizzard Entertainment's Warcraft III: Reign of Chaos and its expansion pack, The Frozen Throne",
+				link          : 'https://en.wikipedia.org/wiki/Dota_2',
+				image         :
+					'https://cdn.cp.adobe.io/content/2/rendition/2fbc681c-bb10-4ddd-9d27-ae0764a88610/version/0/format/jpg/dimension/width/size/200',
+				user_id       : 2,
+				category_name : 'Article',
+				category_id   : '1',
+			},
+			{
+				id            : 3,
+				title         : 'Out of the Box',
+				summary       :
+					"Dota 2 is a multiplayer online battle arena video game developed and published by Valve Corporation. The game is a sequel to Defense of the Ancients, which was a community-created mod for Blizzard Entertainment's Warcraft III: Reign of Chaos and its expansion pack, The Frozen Throne",
+				link          : 'https://en.wikipedia.org/wiki/Dota_2',
+				image         :
+					'https://99designs-blog.imgix.net/blog/wp-content/uploads/2017/12/attachment_83090027.jpg?auto=format&q=60&fit=max&w=930',
+				user_id       : 2,
+				category_name : 'Research',
+				category_id   : '1',
+			},
+			{
+				id            : 4,
+				title         : 'A Time of Witches',
+				summary       :
+					"Dota 2 is a multiplayer online battle arena video game developed and published by Valve Corporation. The game is a sequel to Defense of the Ancients, which was a community-created mod for Blizzard Entertainment's Warcraft III: Reign of Chaos and its expansion pack, The Frozen Throne",
+				link          : 'https://en.wikipedia.org/wiki/Dota_2',
+				image         :
+					'https://images.squarespace-cdn.com/content/v1/5202d1b3e4b099a0812c51a3/1538105761516-V8KW9R54U0RGBERXKV44/ke17ZwdGBToddI8pDm48kN4oao1PlpO_GApQgaaGf-tZw-zPPgdn4jUwVcJE1ZvWEtT5uBSRWt4vQZAgTJucoTqqXjS3CfNDSuuf31e0tVFJUL5AUtbP3jMD8Tueu1148GE851RCRrqmyR4NB93uwFW4jCyXLPvvdR287iymYt8/A-Time-of-Witches.jpg',
+				user_id       : 2,
+				category_name : 'Other',
+				category_id   : '1',
+			},
+			{
+				id            : 5,
+				title         : 'Bill & Sparkle',
+				summary       :
+					"Dota 2 is a multiplayer online battle arena video game developed and published by Valve Corporation. The game is a sequel to Defense of the Ancients, which was a community-created mod for Blizzard Entertainment's Warcraft III: Reign of Chaos and its expansion pack, The Frozen Throne",
+				link          : 'https://en.wikipedia.org/wiki/Dota_2',
+				image         :
+					'https://img2-placeit-net.s3-accelerate.amazonaws.com/uploads/stage/stage_image/37837/large_thumb_stage.jpg',
+				user_id       : 2,
+				category_name : 'Book',
+				category_id   : '1',
+			},
+			{
+				id            : 6,
+				title         : 'Set for Life',
+				summary       :
+					"Dota 2 is a multiplayer online battle arena video game developed and published by Valve Corporation. The game is a sequel to Defense of the Ancients, which was a community-created mod for Blizzard Entertainment's Warcraft III: Reign of Chaos and its expansion pack, The Frozen Throne",
+				link          : 'https://en.wikipedia.org/wiki/Dota_2',
+				image         : 'https://damonza.com/wp-content/uploads/portfolio/nonfiction/Set%20For%20Life%202.jpg',
+				user_id       : 2,
+				category_name : 'Research',
+				category_id   : '1',
+			},
+			{
+				id            : 7,
+				title         : 'Creative Business Startup',
+				summary       :
+					"Dota 2 is a multiplayer online battle arena video game developed and published by Valve Corporation. The game is a sequel to Defense of the Ancients, which was a community-created mod for Blizzard Entertainment's Warcraft III: Reign of Chaos and its expansion pack, The Frozen Throne",
+				link          : 'https://en.wikipedia.org/wiki/Dota_2',
+				image         :
+					'https://d1csarkz8obe9u.cloudfront.net/posterpreviews/non-fiction-business-kindle-book-cover-design-template-2fac9a2b8a04f729d06c809ad50214b6.jpg?ts=1561422624',
+				user_id       : 2,
+				category_name : 'Other',
+				category_id   : '1',
+			},
+			{
+				id            : 8,
+				title         : 'The little Mermaid',
+				summary       :
+					"Dota 2 is a multiplayer online battle arena video game developed and published by Valve Corporation. The game is a sequel to Defense of the Ancients, which was a community-created mod for Blizzard Entertainment's Warcraft III: Reign of Chaos and its expansion pack, The Frozen Throne",
+				link          : 'https://en.wikipedia.org/wiki/Dota_2',
+				image         :
+					'https://img0-placeit-net.s3-accelerate.amazonaws.com/uploads/stage/stage_image/37836/large_thumb_stage.jpg',
+				user_id       : 2,
+				category_name : 'Article',
+				category_id   : '1',
+			},
+		]);
+	});
 };
